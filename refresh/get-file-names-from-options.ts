aa2803577988a82879a26cb54742f4bacd49364f
itisem <emily@itisem.com>
2024-02-01 20:04:30 +0100

    get file names from options

---
+++ refresh/get-file-names-from-options.ts
@@ -0,0 +1,35 @@
+import {defaultRelease} from "./get-release.js";
+
+interface RefreshOptions{
+	// should oceans be included in the geojson? default: true
+	includeOceans?: boolean;
+	// release id. if not set, get it from github or fallback to default
+	release?: string;
+	// release type. for values, see https://github.com/evansiroky/timezone-boundary-builder. default: "all"
+	since?: "1970" | "all" | "now";
+}
+
+interface FileNames{
+	url: string;
+	unzipped: string;
+	tmp: string;
+}
+
+export default function getFileNamesFromOptions(options?: RefreshOptions): FileNames{
+	// set default release again, if something somehow broke greatly
+	const release = options?.release ?? defaultRelease;
+	// if oceans are included, "-with-oceans" is appended, otherwise, nothing is
+	const oceanText = (options?.includeOceans ?? true) ? "-with-oceans" : "";
+	// default to all timezones
+	const since = options?.since ?? "all";
+	// append appropriate text to type names
+	const typeText = since === "1970" ? "-1970" : (since === "now" ? "-now" : "");
+	// get the file url
+	const url = `https://github.com/evansiroky/timezone-boundary-builder/releases/download/${release}/timezones${oceanText}${typeText}.geojson.zip`;
+	// get the unzipped filename
+	const unzipped = `combined${oceanText}${typeText}.json`;
+	// get a temporary filename for the zip. doesn't depend on options, but it's nice to have around in one helper function
+	const zip = `tmp-${Date.now()}.zip`;
+
+	return {url, unzipped, zip};
+}
---
+++ tests/refresh/get-file-names-from-options.ts
@@ -0,0 +1,40 @@
+import getFileNamesFromOptions from "../../refresh/get-file-names-from-options";
+
+describe("getFileNamesFromOptions", () => {
+	test("no options", () => {
+		expect(getFileNamesFromOptions()).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2023d/timezones-with-oceans.geojson.zip",
+			unzipped: "combined-with-oceans.json"
+		});
+	});
+	test("1970", () => {
+		expect(getFileNamesFromOptions({release: "2022a", since: "1970"})).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2022a/timezones-with-oceans-1970.geojson.zip",
+			unzipped: "combined-with-oceans-1970.json"
+		});
+	});
+	test("now", () => {
+		expect(getFileNamesFromOptions({release: "2022a", since: "now"})).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2022a/timezones-with-oceans-now.geojson.zip",
+			unzipped: "combined-with-oceans-now.json"
+		});
+	});
+	test("no oceans", () => {
+		expect(getFileNamesFromOptions({release: "2022a", includeOceans: false})).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2022a/timezones.geojson.zip",
+			unzipped: "combined.json"
+		});
+	});
+	test("no oceans 1970", () => {
+		expect(getFileNamesFromOptions({release: "2022a", since: "1970", includeOceans: false})).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2022a/timezones-1970.geojson.zip",
+			unzipped: "combined-1970.json"
+		});
+	});
+	test("no oceans now", () => {
+		expect(getFileNamesFromOptions({release: "2022a", since: "now", includeOceans: false})).toMatchObject({
+			url: "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2022a/timezones-now.geojson.zip",
+			unzipped: "combined-now.json"
+		});
+	});
+});
